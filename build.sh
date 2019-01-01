35d11f9fd396741b5417f8e8abc7658080c78e16
sathish-vel <veluchamysathishkumar@gmail.com>
2019-01-01 13:30:57 +1300

    build script changes

--- build.sh
+++ build.sh
@@ -3,3 +3,10 @@
 
 php vendor/silverstripe/framework/cli-script.php dev/build "flush=1"
 
+# validate exit status
+if [ "$?" -eq "0" ]; then
+  echo "Cheers! Your Build is Successfull"
+  exit 0
+else
+  echo "Your Build is failed, have a look at your error.log. Incase you havent created error log, please do add it in .env"
+fi
